6cbb6c4f900864841924f6355b91787e44d98be7
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-23 13:04:43 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // 👇 이 부분이 없어서 에러가 난 것입니다!
-  site: 'https://the-besedka-loop.vercel.app', 
-  
-  // 👇 사이트맵 플러그인 추가
-  integrations: [sitemap()],
+  site: 'https://the-besedka-loop.vercel.app',
 });
