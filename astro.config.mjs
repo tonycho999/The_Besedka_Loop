f1c07488ff930dd927eb07a6093e281f027b8263
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-23 12:58:33 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // 1. 이 줄 추가
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://the-besedka-loop.vercel.app', // 2. 본인의 배포된 사이트 주소 입력 (필수!)
+  integrations: [sitemap()], // 3. 이 줄 추가
+});
